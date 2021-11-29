0c8fdde5fbb2a520452c42b4cd1acd0b91f5bba3
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-11-29 15:52:04 -0800

    Server up and running, starting on endpoints in server.js

--- api/server.js
+++ api/server.js
@@ -1,3 +1,15 @@
 // BUILD YOUR SERVER HERE
+// ---------- IMPORTS ---------- 
+const express = require('express'); // Check node_modules folder looking for express
+const users = require('./users/model'); // These functions are call the database
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// ---------- INSTANCE OF EXPRESS APP ---------- 
+const server = express();
+
+// ---------- GLOBAL MIDDLEWARE ---------- 
+server.use( express.json() );
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
